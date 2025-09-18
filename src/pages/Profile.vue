991071519965d38b6217f3acee7ee39f5adb417b
Peerapat Padtawaro <peerapat.p@houseofdev.tech>
2025-09-18 09:50:55 +0700

    Fix layouts

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,13 +1,10 @@
 <template>
-    <div>
-
+  <div class="flex justify-center items-center p-6 w-full min-h-screen">
+    <div class="bg-white rounded-xl shadow-2xl p-6 sm:p-8 text-center max-w-xs w-full border border-gray-200">
+        test
     </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
-
-<style lang="scss" scoped>
-
-</style>
